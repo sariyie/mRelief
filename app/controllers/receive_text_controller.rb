29d1d9c0f361310ffc070a86a02706e07415aa8b
student <student@TSL-MBP-06.local>
2014-08-28 21:03:40 -0500

    Updated gem

--- app/controllers/receive_text_controller.rb
+++ app/controllers/receive_text_controller.rb
@@ -1,8 +1,7 @@
-class ReceiveTextController < ActionController
+class ReceiveTextController < ApplicationController
   def index
     # let's pretend that we've mapped this action to
     # http://localhost:3000/sms in the routes.rb file
-    http://mrelief.herokuapp.com/
 
     message_body = params["Body"]
     from_number = params["From"]
