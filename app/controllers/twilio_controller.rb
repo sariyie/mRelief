dd13c9454029681f88cb46a33625c5e92a8d258a
Rose <sariyie@gmail.com>
2014-08-30 18:41:09 -0500

    Building on code in twilio controller in the hopes of having a text session

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,11 +8,22 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def text
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Welcome to mRelief! This conversation will help determine your eligibility for food stamps. How old are you?"
+    # twiml = Twilio::TwiML::Response.new do |r|
+    #   r.Message "Welcome to mRelief! This conversation will help determine your eligibility for food stamps. How old are you?"
+    # end
+    session["counter"] ||= 0
+    sms_count = session["counter"]
+    if sms_count == 0
+      message = "Hello, thanks for the new message."
+    else
+      message = "Hello, thanks for message number #{sms_count + 1}"
     end
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message message
+    end
+    session["counter"] += 1
     twiml.text
-    render_twiml twiml
+      render_twiml twiml
 
   end
 end
