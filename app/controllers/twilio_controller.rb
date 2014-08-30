6dea89d16b493a8ed33096271af6d9ea3da1f0f3
Rose <sariyie@gmail.com>
2014-08-30 18:12:10 -0500

    Removed session

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,18 +8,15 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def text
-    session["counter"] ||= 0
-    sms_count = session["counter"]
     message = nil
     if sms_count == 0
       message = "Hello, thanks for the new message."
     else
-      message = "Hello, thanks for message number #{sms_count + 1}"
+      message = "Hello, thanks for message number"
     end
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message message
     end
-    session["counter"] += 1
     twiml.text
     response_twiml twiml
 
