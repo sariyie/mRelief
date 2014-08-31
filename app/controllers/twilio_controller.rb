acb8ff08e20363ad334e93f84b6d4efdd4e89fe4
Rose <sariyie@gmail.com>
2014-08-31 18:23:09 -0500

    Fixed errors with equal signs in Twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -13,18 +13,18 @@ class TwilioController < ApplicationController
     # end
     session["counter"] ||= 0
     sms_count = session["counter"]
-    # if params[:body] == "reset"
-    #   sms_count == 0
-    # end
+    if params[:body] == "reset"
+      sms_count == 0
+    end
     if sms_count == 0
       message = "Welcome to mRelief! This conversation will help determine your eligibility for food stamps. How old are you?"
-    elsif sms_count = 1
+    elsif sms_count == 1
       session["age"] = params[:body]
       message = "How many dependents are in your household? Dependents are the number of people living in your household, including yourself. #{session['counter']}"
-    elsif sms_count = 2
+    elsif sms_count == 2
       session["dependents"] = params[:body]
       message = "What is your gross monthly income? Income includes social security, child support, and unemployment insurance before any deductions."
-    elsif sms_count = 3
+    elsif sms_count == 3
       session["income"] = params[:body]
       message = "Your income is #{session['income']}. You have #{session['dependents']}. You are #{session['age']}"
     else
