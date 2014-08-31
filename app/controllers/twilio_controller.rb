472febb3a3493487d3bb35fef34244635a92c910
Rose <sariyie@gmail.com>
2014-08-30 19:05:23 -0500

    Successfully added previous twilio message. Thus, building on that message with a session that reflects foodstamp questions.

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -13,10 +13,21 @@ class TwilioController < ApplicationController
     # end
     session["counter"] ||= 0
     sms_count = session["counter"]
+    if params[:body] == "reset"
+      sms_count == 0
+    end
     if sms_count == 0
-      message = "Hello, thanks for the new message."
+      message = "Welcome to mRelief! This conversation will help determine your eligibility for food stamps. How old are you?"
+    elsif sms_count = 1
+      session["age"] = params[:body]
+      message = "How many dependents are in your household? Dependents are the number of people living in your household, including yourself."
+    elsif sms_count = 2
+      session["dependents"] = params[:body]
+      message = "What is your gross monthly income? Income includes social security, child support, and unemployment insurance before any deductions."
+    elsif sms_count = 3
+      session["income"] = params[:body]
+      message = "Your income is #{session['income']}. You have #{session['dependents']}. You are #{session['age']}"
     else
-      message = "Hello, thanks for message number #{sms_count + 1}"
     end
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message message
