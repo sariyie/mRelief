40fd263e11d718b7e53ac2bc507f2f487dc045cb
Rose <sariyie@gmail.com>
2014-08-31 18:41:55 -0500

    changed if-statements in twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -18,13 +18,16 @@ class TwilioController < ApplicationController
     end
     if sms_count == 0
       message = "Welcome to mRelief! This conversation will help determine your eligibility for food stamps. How old are you?"
-    elsif sms_count == 1
+    end
+    if sms_count == 1
       session["age"] = params[:body]
       message = "How many dependents are in your household? Dependents are the number of people living in your household, including yourself. #{session['counter']}"
-    elsif sms_count == 2
+    end
+    if sms_count == 2
       session["dependents"] = params[:body]
       message = "What is your gross monthly income? Income includes social security, child support, and unemployment insurance before any deductions."
-    elsif sms_count == 3
+    end
+    if sms_count == 3
       session["income"] = params[:body]
       message = "Your income is #{session['income']}. You have #{session['dependents']}. You are #{session['age']}"
     else
