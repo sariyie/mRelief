39624a8a1f3fcd58bc0f9f8866c5c9cb25b5287b
Rose <sariyie@gmail.com>
2014-08-31 19:10:37 -0500

    Fixed capitalizations in twilio controller  - body needs to be Body

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -13,26 +13,26 @@ class TwilioController < ApplicationController
     # end
     session["counter"] ||= 0
     sms_count = session["counter"]
-    if params[:body] == "reset"
+    if params[:Body] == "reset"
       sms_count = 0
     end
     if sms_count == 0
       message = "Welcome to mRelief! This conversation will help determine your eligibility for food stamps. How old are you?"
     end
     if sms_count == 1
-      session["age"] = params[:body]
+      session["age"] = params[:Body]
       message = "How many dependents are in your household? Dependents are the number of people living in your household, including yourself. #{session['counter']}"
     end
     if sms_count == 2
-      session["dependents"] = params[:body]
+      session["dependents"] = params[:Body]
       message = "What is your gross monthly income? Income includes social security, child support, and unemployment insurance before any deductions."
     end
     if sms_count == 3
-      session["income"] = params[:body]
+      session["income"] = params[:Body]
       message = "Your income is #{session['income']}. You have #{session['dependents']}. You are #{session['age']}"
     end
     if sms_count > 3
-      message = "Still stuck #{params[:body]} #{session['counter']}"
+      message = "Still stuck #{params[:Body]} #{session['counter']}"
     end
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message message
